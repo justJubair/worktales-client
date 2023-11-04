896dafcc03737955cb827bbe7a54e7992a442832
Jubair <jubair.ahmed7946@gmail.com>
2023-11-05 01:23:43 +0600

    Add: jobs loaded based on category

--- src/components/BrowseCategory/BrowseCategory.jsx
+++ src/components/BrowseCategory/BrowseCategory.jsx
@@ -1,11 +1,35 @@
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import useAxios from "../../hooks/useAxios";
+import { useEffect, useState } from "react";
 
 const BrowseCategory = () => {
+    const [category, setCategory] = useState("webDevelopment")
+    const [jobs, setjobs] = useState([])
+    const axios = useAxios()
+    useEffect(()=>{
+        axios.get(`/jobs?category=${category}`)
+        .then(res=>{
+            setjobs(res.data)
+        })
+    },[axios, category])
+   
+    console.log(jobs)
+    const handleCategory =e=>{
+        const selectedCategory =  e.target.textContent
+        if(selectedCategory === "Digital Marketing"){
+            setCategory("digitalMarketing")
+          
+        } else if(selectedCategory === "Graphics Design"){
+            setCategory("graphicsDesigning")
+           
+        }
+    }
+    
   return (
     <div>
       <Tabs className={"text-center"}>
-        <TabList>
+        <TabList onClick={handleCategory}>
           <Tab>Web Development</Tab>
           <Tab>Digital Marketing</Tab>
           <Tab>Graphics Design</Tab>

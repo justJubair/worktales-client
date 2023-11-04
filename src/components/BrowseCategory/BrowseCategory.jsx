a618e33d60585989d44f36e2d916617cf6268dde
Jubair <jubair.ahmed7946@gmail.com>
2023-11-05 02:00:27 +0600

    Add: category based jobs dipslayed in the UI

--- src/components/BrowseCategory/BrowseCategory.jsx
+++ src/components/BrowseCategory/BrowseCategory.jsx
@@ -2,30 +2,29 @@ import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import useAxios from "../../hooks/useAxios";
 import { useEffect, useState } from "react";
+import SingleJob from "./SingleJob";
 
 const BrowseCategory = () => {
-    const [category, setCategory] = useState("webDevelopment")
-    const [jobs, setjobs] = useState([])
-    const axios = useAxios()
-    useEffect(()=>{
-        axios.get(`/jobs?category=${category}`)
-        .then(res=>{
-            setjobs(res.data)
-        })
-    },[axios, category])
-   
-    console.log(jobs)
-    const handleCategory =e=>{
-        const selectedCategory =  e.target.textContent
-        if(selectedCategory === "Digital Marketing"){
-            setCategory("digitalMarketing")
-          
-        } else if(selectedCategory === "Graphics Design"){
-            setCategory("graphicsDesigning")
-           
-        }
+  const [category, setCategory] = useState("webDevelopment");
+  const [jobs, setjobs] = useState([]);
+  const axios = useAxios();
+  useEffect(() => {
+    axios.get(`/jobs?category=${category}`).then((res) => {
+      setjobs(res.data);
+    });
+  }, [axios, category]);
+
+  const handleCategory = (e) => {
+    const selectedCategory = e.target.textContent;
+    if (selectedCategory === "Digital Marketing") {
+      setCategory("digitalMarketing");
+    } else if (selectedCategory === "Graphics Design") {
+      setCategory("graphicsDesigning");
+    } else {
+      setCategory("webDevelopment");
     }
-    
+  };
+
   return (
     <div>
       <Tabs className={"text-center"}>
@@ -35,15 +34,15 @@ const BrowseCategory = () => {
           <Tab>Graphics Design</Tab>
         </TabList>
 
-        <TabPanel>
-          <h2>Any content 1</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 3</h2>
-        </TabPanel>
+        {Array(3)
+          .fill(0)
+          .map((_, idx) => (
+            <TabPanel key={idx}>
+              {jobs?.map((job) => (
+                <SingleJob key={job._id} job={job} />
+              ))}
+            </TabPanel>
+          ))}
       </Tabs>
     </div>
   );
---
+++ src/components/BrowseCategory/SingleJob.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+const SingleJob = ({job}) => {
+    return(
+        <div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="text-xl font-bold text-center text-[#a55b5b]">{job.job_title}</h2>
+    <div className="text-start space-y-2 mt-6">
+    <p><span className="font-medium">Deadline:</span> {job.deadline}</p>
+    <p><span className="font-medium">Price:</span> {job.price_range}</p>
+    <p><span className="font-medium">Description:</span> {job.description}</p>
+    </div>
+    <div className="card-actions justify-end">
+      <button className="btn bg-[#4b1818]">Bid Now</button>
+    </div>
+  </div>
+</div>
+    )}
+
+SingleJob.propTypes = {
+  job:PropTypes.object,
+}    
+export default SingleJob;

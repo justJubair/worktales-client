de9326986bd7188fab4ac740cd5ceffcf5d0b3cf
Jubair <jubair.ahmed7946@gmail.com>
2023-11-04 21:19:24 +0600

    Add: banner background img added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,9 @@
 const Banner = () => {
     return(
-        <div>
-             <p> HELLO I Am Banner </p>
+        <div className="h-screen w-full">
+           <img className="h-full object-cover w-full" src="https://i.ibb.co/Ytq7sj6/map-lying-wooden-table.jpg" alt="" />
+           {/* overlay */}
+           <div className="h-screen absolute left-0 top-0 bg-[#160202]/90 w-full"></div>
         </div>
     )}
 export default Banner;

d561e5948115e7afcfcf70036dbb2a9b72690a1d
Jubair <jubair.ahmed7946@gmail.com>
2023-11-05 23:03:57 +0600

    Add: login page added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,15 +1,21 @@
 import { createContext } from "react";
 import PropTypes from "prop-types"
 import auth from "../config/firebase.config";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
     // Create new users
     const signUp = (email,password)=>{
         return createUserWithEmailAndPassword(auth, email, password)
     }
+
+    // login existing users
+    const login =(email, password)=>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
     const authInfo = {
         signUp,
+        login,
     }
     return(
         <AuthContext.Provider value={authInfo}>

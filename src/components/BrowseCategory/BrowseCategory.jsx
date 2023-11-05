09e64bc214945e0fdfacbb98fd2b994fa8b0d553
Jubair <jubair.ahmed7946@gmail.com>
2023-11-05 13:02:40 +0600

    Add: jobs card designed

--- src/components/BrowseCategory/BrowseCategory.jsx
+++ src/components/BrowseCategory/BrowseCategory.jsx
@@ -26,21 +26,24 @@ const BrowseCategory = () => {
   };
 
   return (
-    <div>
-      <Tabs className={"text-center"}>
-        <TabList onClick={handleCategory}>
-          <Tab>Web Development</Tab>
-          <Tab>Digital Marketing</Tab>
-          <Tab>Graphics Design</Tab>
+    <div className="max-w-screen-xl mx-auto">
+      <Tabs className={"text-center my-16"}>
+        <TabList  onClick={handleCategory}>
+          <Tab ><h3 className="bg-[#4b1818] text-white rounded-md px-4 py-2">Web Development</h3></Tab>
+          <Tab><h3 className="bg-[#4b1818] text-white rounded-md px-4 py-2">Digital Marketing</h3></Tab>
+          <Tab><h3 className="bg-[#4b1818] text-white rounded-md px-4 py-2">Graphics Design</h3></Tab>
         </TabList>
 
         {Array(3)
           .fill(0)
           .map((_, idx) => (
             <TabPanel key={idx}>
+            <div className="grid gap-4 grid-cols-1 mt-10 md:grid-cols-2">
+
               {jobs?.map((job) => (
                 <SingleJob key={job._id} job={job} />
               ))}
+            </div>
             </TabPanel>
           ))}
       </Tabs>
--- src/components/BrowseCategory/SingleJob.jsx
+++ src/components/BrowseCategory/SingleJob.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 const SingleJob = ({job}) => {
     return(
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-full bg-base-100 shadow-xl">
   <div className="card-body">
     <h2 className="text-xl font-bold text-center text-[#a55b5b]">{job.job_title}</h2>
     <div className="text-start space-y-2 mt-6">
